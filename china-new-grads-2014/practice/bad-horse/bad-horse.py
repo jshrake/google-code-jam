1689ef1c3d7d2dbc090052b41255814ebaff3c19
justinshrake <justinshrake@gmail.com>
2013-11-29 20:23:24 -0800

    added some comments

--- china-new-grads-2014/practice/bad-horse/bad-horse.py
+++ china-new-grads-2014/practice/bad-horse/bad-horse.py
@@ -2,6 +2,10 @@ import sys
 
 def is_bipartite(graph, v, color, coloring):
   coloring[v] = color 
+  # iterate through adjacent nodes and look for any nodes
+  # that have a coloring that does not match our expectation
+  # that adjacent nodes have an opposite color of our current
+  # node
   for node in graph[v]:
     if node not in coloring and not is_bipartite(graph, node, not color, coloring):
       return False
@@ -19,6 +23,9 @@ for case in range(1, num_cases + 1):
     graph.setdefault(names[1], []).append(names[0])
   coloring = {}
   output = "Yes"
+  # iterate through all nodes in the graph
+  # graph may not be connected
+  # look for any indication that the graph is not bipartite
   for node in graph.keys():
     if node not in coloring and not is_bipartite(graph, node, True, coloring):
         output = "No"
