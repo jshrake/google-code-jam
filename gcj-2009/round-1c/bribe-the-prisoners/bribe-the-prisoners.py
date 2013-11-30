3682a0995ea30e16d874b8535c4934a17b8dc59e
justinshrake <justinshrake@gmail.com>
2013-11-30 07:10:52 -0800

    remove the duplicate insertion into the memoized cache

--- gcj-2009/round-1c/bribe-the-prisoners/bribe-the-prisoners.py
+++ gcj-2009/round-1c/bribe-the-prisoners/bribe-the-prisoners.py
@@ -8,9 +8,12 @@ def solver(cell_numbers, cell_range):
   if cell_range in mem_solutions:
     return mem_solutions[cell_range]
   coins_for_this_range = cell_range[1] - cell_range[0]
-  mem_solutions[cell_range] = coins_for_this_range
-  cases = []
+  subproblems = []
+  # iterate through each cell
   for cell_number in cell_numbers:
+    # Partition the cell numbers and cell range 
+    # on the current cell number and then solve
+    # two subproblems
     idx = cell_numbers.index(cell_number)
     smaller_range = (cell_range[0], cell_number - 1)
     smaller_cells = cell_numbers[0:idx]
@@ -18,8 +21,10 @@ def solver(cell_numbers, cell_range):
     larger_cells  = cell_numbers[idx+1:]
     smaller_solve  = solver(smaller_cells, smaller_range)
     larger_solve  = solver(larger_cells, larger_range)
-    cases.append(smaller_solve + larger_solve)
-  mem_solutions[cell_range] = coins_for_this_range + min(cases)
+    subproblems.append(smaller_solve + larger_solve)
+  # solution is the current answer + the minimum answer of the 
+  # subproblems we solved
+  mem_solutions[cell_range] = coins_for_this_range + min(subproblems)
   return mem_solutions[cell_range] 
 
 
